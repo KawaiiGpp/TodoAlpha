91b9765e3cce37f434bb1a0e02710d843fefbac7
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-03-13 14:01:06 +0800

    功能增加：可以添加待办事项了。

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_alpha/data/todo.dart';
 import 'package:to_do_alpha/data/todo_list.dart';
 import 'package:to_do_alpha/widget/todo_creation_button.dart';
 import 'package:to_do_alpha/widget/todo_listview.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final TodoList todoList;
 
   const HomePage({super.key, required this.todoList});
 
   @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("待办事项 Alpha")),
-      body: TodoListView(todoList: todoList),
+      body: TodoListView(todoList: widget.todoList),
       floatingActionButton: TodoCreationButton(onCreation: _onTodoCreation),
     );
   }
 
-  void _onTodoCreation(String name) {}
+  void _onTodoCreation(String name) {
+    final todo = Todo.createNew(name);
+    setState(() => widget.todoList.add(todo));
+  }
 }
