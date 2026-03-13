ccab8756735461002dabb1ee0af836b87b7447ea
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-03-13 13:50:57 +0800

    拆解HomePage中的FloatingActionButton。

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_alpha/data/todo.dart';
 import 'package:to_do_alpha/data/todo_list.dart';
+import 'package:to_do_alpha/widget/todo_creation_button.dart';
 import 'package:to_do_alpha/widget/todo_listview.dart';
-import 'package:to_do_alpha/util/dialog.dart';
-import 'package:to_do_alpha/util/light_dialog.dart';
-import 'package:to_do_alpha/util/text_field_dialog.dart';
 
 class HomePage extends StatelessWidget {
   final TodoList todoList;
@@ -16,38 +13,9 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("待办事项 Alpha")),
       body: TodoListView(todoList: todoList),
-      floatingActionButton: _buildTodoCreationButton(context),
+      floatingActionButton: TodoCreationButton(onCreation: _onTodoCreation),
     );
   }
 
-  FloatingActionButton _buildTodoCreationButton(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () => showLightDialog(
-        context,
-
-        pageBuilder: (ctx, a1, a2) {
-          return TextFieldDialog(
-            title: const Text("创建待办事项"),
-            hintText: "请在此输入事项名称 ...",
-            onSubmit: (text) {},
-            onInputCheck: _onInputCheck,
-          );
-        },
-
-        transitionBuilder: (ctx, a1, a2, child) {
-          return LightTransition(sigma: 1.5, animation: a1, child: child);
-        },
-      ),
-
-      elevation: 2,
-      child: const Icon(Icons.add),
-    );
-  }
-
-  String? _onInputCheck(String input) {
-    if (input.isEmpty) return "名称不可为空";
-    return null;
-  }
-
-  void _onNewTodoCreated(Todo todo) {}
+  void _onTodoCreation(String name) {}
 }
---
+++ lib/widget/todo_creation_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_alpha/util/dialog.dart';
+import 'package:to_do_alpha/util/light_dialog.dart';
+import 'package:to_do_alpha/util/text_field_dialog.dart';
+
+class TodoCreationButton extends StatelessWidget {
+  final ValueChanged<String> onCreation;
+
+  const TodoCreationButton({super.key, required this.onCreation});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => showLightDialog(
+        context,
+
+        pageBuilder: (ctx, a1, a2) {
+          return TextFieldDialog(
+            title: const Text("创建待办事项"),
+            hintText: "请在此输入事项名称 ...",
+            onInputCheck: _onInputCheck,
+
+            onSubmit: (text) {
+              Navigator.pop(ctx);
+              onCreation(text);
+            },
+          );
+        },
+
+        transitionBuilder: (ctx, a1, a2, child) {
+          return LightTransition(sigma: 1.5, animation: a1, child: child);
+        },
+      ),
+
+      elevation: 2,
+      child: const Icon(Icons.add),
+    );
+  }
+
+  String? _onInputCheck(String input) {
+    if (input.isEmpty) {
+      return "名称不可为空";
+    }
+
+    return null;
+  }
+}
