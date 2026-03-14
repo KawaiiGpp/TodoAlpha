4d4d0f258c6487c1d834ae3c5c7f65e4efd9a637
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-03-14 11:03:33 +0800

    新增待办事项的详情页。

---
+++ lib/widget/detail_item_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_alpha/util/light_card.dart';
+
+class DetailItemCard extends StatelessWidget {
+  final IconData icon;
+  final String name;
+  final String content;
+
+  const DetailItemCard({
+    super.key,
+    required this.icon,
+    required this.name,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final font = theme.textTheme;
+
+    return LightCard(
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+
+      child: Row(
+        children: [
+          Icon(icon),
+          const SizedBox(width: 12),
+
+          Text(name, style: font.titleMedium),
+          const SizedBox(width: 24),
+
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: .horizontal,
+              reverse: true,
+
+              child: Text(content, style: font.titleMedium),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widget/todo_detail_panel.dart
+++ lib/widget/todo_detail_panel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_alpha/data/todo.dart';
+import 'package:to_do_alpha/util/constants.dart';
+import 'package:to_do_alpha/widget/detail_item_card.dart';
 
 class TodoDetailPanel extends StatelessWidget {
   final Todo todo;
@@ -8,6 +10,28 @@ class TodoDetailPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+
+      children: [
+        DetailItemCard(
+          icon: Icons.font_download,
+          name: "名称",
+          content: todo.name,
+        ),
+
+        DetailItemCard(
+          icon: Icons.event_available,
+          name: "创建日期",
+          content: dateFormat.format(todo.createTime),
+        ),
+
+        DetailItemCard(
+          icon: Icons.check_box,
+          name: "已完成",
+          content: todo.completed ? "是" : "否",
+        ),
+      ],
+    );
   }
 }
